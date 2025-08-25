3e7d9efda7e5cc4dc6e06dd752e081d793d5b8d2
patil-jayesh123 <jp5787625@gmail.com>
2025-08-25 14:45:46 +0530

    Your new changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,18 +3,18 @@ import ProductsNo from "./ProductsNo";
 
 
 let myProducts = [
-    { id: 0, name: "iphone", img: "/public/iphone.jpg", price: "1,30,000" },
-    { id: 0, name: "facewash", img: "/public/facewash.webp", price: "500" },
-    { id: 0, name: "headphone", img: "/public/headphone.webp", price: "1000" },
-    { id: 0, name: "washing", img: "/public/washing.webp", price: "10,000" },
-    { id: 0, name: "Sandal", img: "/public/sandal.jpg", price: "300" },
-    { id: 0, name: "watch", img: "/public/watch.jpg", price: "700" },
-    { id: 0, name: "T-shirt", img: "/public/t-shirt.avif", price: "600" },
-    { id: 0, name: "Nike shoes", img: "/public/nike.avif", price: "10,000" },
-    { id: 0, name: "Moisturizer", img: "/public/moisturizer.avif", price: "500" },
-    { id: 0, name: "Cycle", img: "/public/cycle.jpg ", price: "8000" },
-    { id: 0, name: "child dress", img: "/public/clothing.jpg ", price: "2000" },
-    { id: 0, name: "Kurti", img: "/public/g1.webp ", price: "900" },
+    { id: 0, name: "iphone", img: "./src/assets/iphone.jpg", price: "1,30,000" },
+    { id: 0, name: "facewash", img: "./src/assets/facewash.webp", price: "500" },
+    { id: 0, name: "headphone", img: "./src/assets/headphone.webp", price: "1000" },
+    { id: 0, name: "washing", img: "./src/assets/washing.webp", price: "10,000" },
+    { id: 0, name: "Sandal", img: "./src/assets/sandal.jpg", price: "300" },
+    { id: 0, name: "watch", img: "./src/assets/watch.jpg", price: "700" },
+    { id: 0, name: "T-shirt", img: "./src/assets/t-shirt.avif", price: "600" },
+    { id: 0, name: "Nike shoes", img: "./src/assets/nike.avif", price: "10,000" },
+    { id: 0, name: "Moisturizer", img: "./src/assets/moisturizer.avif", price: "500" },
+    { id: 0, name: "Cycle", img: "./src/assets/cycle.jpg ", price: "8000" },
+    { id: 0, name: "child dress", img: "./src/assets/clothing.jpg ", price: "2000" },
+    { id: 0, name: "Kurti", img: "./src/assets/g1.webp ", price: "900" },
     
     ];
 
